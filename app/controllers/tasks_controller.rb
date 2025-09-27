d74dd77ab719b68fe7357854fd24c35a6eca5c3b
Udit Takkar <udit222001@gmail.com>
2025-09-28 03:05:56 +0530

    feat: add basic

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+    before_action :require_login
+
+    def index
+        @tasks = current_user.tasks
+    end
+
+    def show
+        @task = current_user.tasks.find(params[:id])
+    end
+
+    def new 
+        @task = current_user.tasks.new
+    end
+
+    def create
+        @task = current_user.tasks.new(task_params)
+        if @task.save
+            redirect_to tasks_path
+        else 
+            render :new
+        end
+    end
+
+    def edit
+        @task = current_user.tasks.find(params[:id])
+    end
+
+    def update
+        @task = current_user.tasks.find(params[:id])
+        if @task.update(task_params)
+            redirect_to tasks_path
+        else 
+            render :edit
+        end
+    end
+
+    def destroy
+        @task = current_user.tasks.find(params[:id])
+        @task.destroy
+        redirect_to tasks_path
+    end
+
+    private 
+
+    def task_params
+        params.require(:task).permit(:title, :description, :completed)
+    end
+
+end
